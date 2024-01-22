7a7d3a90983d8fc5c0f332dcc9c5b5917e6d0c11
Hanner Enrique De La Hoz Barraza <hdelahoz@lya-electronic.com>
2024-01-22 17:14:20 -0500

    [FEAT] swagger config

--- src/main/java/com/example/demo/models/Client.java
+++ src/main/java/com/example/demo/models/Client.java
@@ -1,24 +1,33 @@
 package com.example.demo.models;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity()
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity()
 @Table(name = "clients")
 public class Client {
 
+    @Schema(hidden = true)
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id", length = 36)
     private String id;
 
+    @Schema(defaultValue = "Hanner")
     @Column(name = "name")
     private String name;
 
+    @Schema(defaultValue = "De La Hoz")
     @Column(name = "lastname")
     private String lastname;
 
+    @Schema(defaultValue = "23")
     @Column(name = "age")
     private Integer age;
 
